6f9179153c0e2f078f115a8b4e39a673dc0ac4b3
Somashekar CG <somu.birugandi.kattimane.03@gmail.com>
2024-04-12 21:57:23 +0530

    Add project files.

---
+++ WebApplication1/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public string Ename { get; set; }
+    }
+}
